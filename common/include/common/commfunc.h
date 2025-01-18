80df2c4ede08997716f67330a24d0caf4392cbd5
abin <abin.z@outlook.com>
2025-01-19 00:43:03 +0800

    feat: 添加关于extern C 的注释

--- common/include/common/commfunc.h
+++ common/include/common/commfunc.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <common/dll_export.h>
 
+/* 
+ * 使用 extern "C" 的好处:
+ * 避免 C++ 名称修饰, 使用 C 风格的函数接口, 确保了 符号命名一致性 和 函数调用约定一致性.
+ * 使动态库对 C 和其他语言友好，增强了ABI兼容性。
+ * 其他语言（如 C、Python、Java）调用这些接口时，必须使用一致的 ABI。
+*/
 #ifdef __cplusplus
 extern "C"
 {
